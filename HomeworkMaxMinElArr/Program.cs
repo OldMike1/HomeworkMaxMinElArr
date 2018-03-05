2ac949bfc2cf09c377e12ce46d87d598833b2108
mike19952006 <mike19952006@gmail.com>
2018-03-05 16:16:51 +0200

    Мин. и макс. значения массива, их разница и позиции в массиве.

--- HomeworkMaxMinElArr/Program.cs
+++ HomeworkMaxMinElArr/Program.cs
@@ -10,6 +10,41 @@ namespace HomeworkMaxMinElArr
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the length of the array");
+            int x = int.Parse(Console.ReadLine());
+            int[] array = new int[x];
+            Random rnd = new Random();
+            int Max = 0;
+            int Min = 10;
+            string IndexForMax = "";
+            string IndexForMin = "";
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(1, 10);
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (Max <= array[i])
+                    Max = array[i];
+
+                if (Min >= array[i])
+                    Min = array[i];
+                
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (Max == array[i])
+                    IndexForMax = IndexForMax + i + " ";
+
+                if (Min == array[i])
+                    IndexForMin = IndexForMin + i + " ";
+            }
+            Console.WriteLine($"Max = {Max} in positions {IndexForMax}");
+            Console.WriteLine($"Min = {Min} in positions {IndexForMin}");
+            int difference = Max - Min;
+            Console.WriteLine($"difference = {difference}");
         }
     }
 }
